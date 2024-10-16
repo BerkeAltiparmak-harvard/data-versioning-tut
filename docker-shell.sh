be4f5ff32343faf7cfd718dc9cbbc18da4d80a5e
BerkeAltiparmak <altiparmak.berke@gmail.com>
2024-10-16 23:10:30 +0000

    dataset updates...

--- docker-shell.sh
+++ docker-shell.sh
@@ -4,10 +4,11 @@ set -e
 
 export BASE_DIR=$(pwd)
 export SECRETS_DIR=$(pwd)/../secrets/
-export GCS_BUCKET_NAME="cheese-app-data-versioning"
-export GCP_PROJECT="ac215-project"
+export GCS_BUCKET_NAME="runningassistant"
+export GCP_PROJECT="runningassistant-438704"
 export GCP_ZONE="us-central1-a"
-export GOOGLE_APPLICATION_CREDENTIALS="/secrets/data-service-account.json"
+export GOOGLE_APPLICATION_CREDENTIALS="/secrets/runningassistant-438704-172173051455.json"
+
 
 
 echo "Building image"
